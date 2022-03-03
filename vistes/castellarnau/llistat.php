f3afe0d48a05c9663e36a7f80d9274333b489b2c
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-03-03 21:52:17 +0100

    Configurar botón de Previous y Next

--- vistes/castellarnau/llistat.php
+++ vistes/castellarnau/llistat.php
@@ -21,7 +21,7 @@
 
     foreach ($res as $fragmentos) {
 
-        echo "<br><br><a href='index.php?control=ControlCastellarnau&operacio=getFrase&codi=". $fragmentos['ID'] ."'>" . $fragmentos['frase'] . "</a><br><br></td>";
+      echo "<br><br><a href='index.php?control=ControlCastellarnau&operacio=getFrase&codi=" . $fragmentos['ID'] . "'>" . $fragmentos['frase'] . "</a><br><br></td>";
 
       echo "</tr>";
     }
@@ -36,24 +36,27 @@
     <nav>
       <ul class="pagination">
         <?php
-        echo "<li class='page-item'><span class='page-link'>Previous</span></li>";
+
+        $next = $numPagina + 1;
+        $previous = $numPagina - 1;
+
+
+        echo "<li class='page-item'><a class='page-link' href='index.php?control=ControlCastellarnau&operacio=llistat&page=$previous'>Previous</a></li>";
         for ($i = 1; $i <= $total_pags; $i++) {
 
           echo "<li class='page-item'><a class='page-link' href='index.php?control=ControlCastellarnau&operacio=llistat&page=" . $i . "' >" . $i . "</a></li>";
-        
         }
-
-        $next = $i + 1;
 
         echo " <li class='page-item'><a class='page-link' href='index.php?control=ControlCastellarnau&operacio=llistat&page=$next'>Next</a></li>";
 
         ?>
 
       </ul>
-    </nav> 
+    </nav>
 
   </div>
 
- 
+
 </body>
+
 </html>
